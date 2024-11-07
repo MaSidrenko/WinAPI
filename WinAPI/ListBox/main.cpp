ba504a66e61e4c5c387d0c16a4aad5014edeea1e
Sidor_md45@top-academy.ru <Sidor_md45@top-academy.ru>
2024-11-07 20:20:56 +0300

    Edti ListBox items DONE

--- WinAPI/ListBox/main.cpp
+++ WinAPI/ListBox/main.cpp
@@ -6,6 +6,8 @@
 CONST CHAR* g_VALUES[] = {"This", "is", "a", "List", "Box"};
 
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+BOOL CALLBACK DlgProcAddItem(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+BOOL CALLBACK DlgProcEditItem(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 INT WINAPI WinMain(HINSTANCE hInstacne, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
 {
@@ -27,6 +29,7 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	case WM_INITDIALOG:
 	{
 		HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
+		//GetModulHandle(NULL) - возвращает hInstance нашей программы
 		SendMessage(hwnd, WM_SETICON, 0, (LPARAM)hIcon);
 
 		HWND hList = GetDlgItem(hwnd, IDC_LIST);
@@ -41,6 +44,13 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	{
 		switch (LOWORD(wParam))
 		{
+		case IDC_LIST:
+			switch (HIWORD(wParam))
+			{
+			case LBN_DBLCLK:
+				DialogBoxParam(GetModuleHandle(NULL), MAKEINTRESOURCE(IDD_DIALOG_ADD), hwnd, DlgProcEditItem, 0);
+				break;
+			}break;
 		case IDOK:
 		{
 			CONST INT SIZE = 256;
@@ -55,14 +65,16 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			MessageBox(hwnd, sz_message, "Info", MB_OK | MB_ICONINFORMATION);
 		}
 		break;
+	
 		case IDCADD:
 		{
-			HWND hEditBox = GetDlgItem(hwnd, IDC_ADD_ELM);
+			/*HWND hEditBox = GetDlgItem(hwnd, IDC_ADD_ELM);
 			HWND hList = GetDlgItem(hwnd, IDC_LIST);
 			CONST INT SIZE = 256;
 			CHAR sz_buffer[SIZE]{};
 			SendMessage(hEditBox, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
-			SendMessage(hList, LB_ADDSTRING, 0, (LPARAM)sz_buffer);
+			SendMessage(hList, LB_ADDSTRING, 0, (LPARAM)sz_buffer);*/
+			DialogBoxParam(GetModuleHandle(NULL), MAKEINTRESOURCE(IDD_DIALOG_ADD), hwnd, DlgProcAddItem, 0);
 		}
 		break;
 		case IDCDELETE:
@@ -72,14 +84,118 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			SendMessage(hList, LB_DELETESTRING, i, 0);
 		}
 		break;
+		case WM_KEYDOWN:
+		{
+			switch (lParam)
+			{
+			case VK_DELETE:
+			{
+				SendMessage(hwnd, WM_COMMAND, LOWORD(IDCDELETE), (LPARAM)GetDlgItem(hwnd, IDC_LIST));
+			}break;
+			}
+		}break;
 		case IDCANCEL: EndDialog(hwnd, 0); break;
-
 		}
 	}
 		break;
 	case WM_CLOSE:
 		EndDialog(hwnd, 0);
 		break;
+	}
+	return FALSE;
+}
+
+BOOL CALLBACK DlgProcAddItem(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_INITDIALOG:
+	{
+		SetFocus(GetDlgItem(hwnd, IDC_EDIT_ADD));
+		
+	}break;
+	case WM_COMMAND:
+	{
+		switch (LOWORD(wParam))
+		{
+		case IDOK:
+		{
+			CONST INT SIZE = 256;
+			CHAR sz_buffer[SIZE]{};
+			HWND hEditAdd = GetDlgItem(hwnd, IDC_EDIT_ADD);
+			SendMessage(hEditAdd, WM_GETTEXT, SIZE, (LPARAM)sz_buffer); \
+
+			HWND hList = GetDlgItem(GetParent(hwnd), IDC_LIST);
+			//GetParanet(hwnd) - Возвращает родительское окно (HWND - родительского окна) для указанного окна.
+			if (strlen(sz_buffer) == 0)
+			{
+				INT result = MessageBox(hwnd, "Вхождение пустое! Хотите ввести другое значение?", "Info", MB_YESNO | MB_ICONINFORMATION);
+				if (result == IDYES)break;
+			}
+			if (SendMessage(hList, LB_FINDSTRING, -1, (LPARAM)sz_buffer) == LB_ERR)
+			{
+				SendMessage(hList, LB_ADDSTRING, 0, (LPARAM)sz_buffer);
+			}
+			else
+			{
+				INT result = MessageBox(hwnd, "Такое вхождение уже есть в списке, хотите ввести другое значение?", "Info", MB_YESNO | MB_ICONQUESTION);
+				if(result == IDYES)break;
+			}
+		}
+		
+		case IDCANCEL:
+			EndDialog(hwnd, 0);
+			break;
+		}
+	}
+		break;
+	case WM_CLOSE:
+		EndDialog(hwnd, 0);
+		break;
+	}
+	return FALSE;
+}
+
+BOOL CALLBACK DlgProcEditItem(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch(uMsg)
+	{
+	case WM_INITDIALOG:
+	{
+		SendMessage(hwnd, WM_SETTEXT, 0, (LPARAM)"Edit item");
+		HWND hParent = GetParent(hwnd);
+		HWND hList = GetDlgItem(hParent, IDC_LIST);
+		HWND hEdit = GetDlgItem(hwnd, IDC_EDIT_ADD);
+		CONST INT SIZE = 256;
+		CHAR sz_buffer[SIZE]{};
+		INT i = SendMessage(hList, LB_GETCURSEL, 0, 0);
+		SendMessage(hList, LB_GETTEXT, i, (LPARAM)sz_buffer);
+		SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)sz_buffer);
+	}
+		break;
+	case WM_COMMAND:
+	{
+
+		switch (LOWORD(wParam))
+		{
+		case IDOK:
+		{
+			HWND hParent = GetParent(hwnd);
+			HWND hList = GetDlgItem(hParent, IDC_LIST);
+			HWND hEdit = GetDlgItem(hwnd, IDC_EDIT_ADD);
+			CONST INT SIZE = 256;
+			CHAR sz_buffer[SIZE]{};
+			SendMessage(hEdit, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
+			INT i = SendMessage(hList, LB_GETCURSEL, 0, 0);
+			SendMessage(hList, LB_DELETESTRING, i, 0);
+			SendMessage(hList, LB_INSERTSTRING, i, (LPARAM)sz_buffer);
+		}
+		case IDCANCEL: EndDialog(hwnd, 0); break;
+		}
+	}
+		break;
+	case WM_CLOSE:
+		EndDialog(hwnd, 0); break;
 	}
 	return FALSE;
 }
--- WinAPI/ListBox/resource.h
+++ WinAPI/ListBox/resource.h
@@ -4,18 +4,21 @@
 //
 #define IDD_DIALOG1                     101
 #define IDI_ICON1                       103
+#define IDD_DIALOG_ADD                  104
 #define IDC_LIST                        1001
 #define IDCADD                          1002
 #define IDC_ADD_ELM                     1004
 #define IDCDELETE                       1005
+#define IDC_EDIT1                       1006
+#define IDC_EDIT_ADD                    1006
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        104
+#define _APS_NEXT_RESOURCE_VALUE        106
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1006
+#define _APS_NEXT_CONTROL_VALUE         1007
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
